fafd048f23af9308a9a63bcf0e32687382f1b237
Roman Khrebtov <roman@alltmb.ru>
2018-07-18 15:50:33 +0300

    Add VCR for tests

--- sealine_insurance.gemspec
+++ sealine_insurance.gemspec
@@ -26,4 +26,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'minitest', '~> 5.0'
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,13 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'sealine_insurance'
 
 require 'minitest/autorun'
+require 'webmock/minitest'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/vcr_cassettes'
+  config.hook_into :webmock
+  config.before_record do |item|
+    item.response.body.force_encoding('UTF-8')
+  end
+end
